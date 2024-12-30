d77b4166555740f11d9542fdc43f79f1eb027f71
Oleksandr <alexandrenko3006@gmail.com>
2024-12-30 13:31:12 -0500

    change auth with accessToken, refreshToken doesn't have access

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,18 +5,18 @@ import { DASHBOARD_PAGES } from "./config/pages-url.config";
  export const middleware = async(request: NextRequest, responce:NextResponse, ) => {
      const { url, cookies } = request
      
-     const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+     const accessToken = cookies.get(EnumTokens.ACCESS_TOKEN)?.value
      const isAuthPage = url.includes('/auth')
      console.log(cookies)
   
      
-     if (isAuthPage && refreshToken) {
+     if (isAuthPage && accessToken) {
          return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
      }
      if (isAuthPage) {
          return NextResponse.next()
      }
-     if (!refreshToken) {
+     if (!accessToken) {
          return NextResponse.redirect(new URL('/auth', request.url))
      }
      return NextResponse.next()
